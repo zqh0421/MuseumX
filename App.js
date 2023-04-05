72deae1b5fb5dcff9eeae945f6a83742151a35ea
zqh0421 <zqh0421@hotmail.com>
2023-04-05 15:26:59 +0800

    Init

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Image, Text, StyleSheet, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function App() {
+
+const ImgPicker = props => {
+  const [pickedImage, setPickedImage] = useState();
+
+  const takeImageHandler = async () => {
+    const image = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5
+    });
+
+    setPickedImage(image.uri);
+    props.onImageTaken(image.uri);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.imagePicker}>
+      <View style={styles.imagePreview}>
+        {!pickedImage ? (
+          <Text>No image picked yet.</Text>
+        ) : (
+          <Image style={styles.image} source={{ uri: pickedImage }} />
+        )}
+      </View>
+      <Button
+        title="Take Image"
+        onPress={takeImageHandler}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  imagePicker: {
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 15
   },
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#ccc',
+    borderWidth: 1
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  }
 });
+
+export default ImgPicker;
