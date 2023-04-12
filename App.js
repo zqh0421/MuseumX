64756c0922e530754c18f502de4f05cf1e128faa
Hachiii123 <111744661+Hachiii123@users.noreply.github.com>
2023-04-12 09:31:30 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,57 +1,44 @@
-import React, { useState } from 'react';
-import { View, Button, Image, Text, StyleSheet, Alert } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, Button } from 'react-native'
 
 
-const ImgPicker = props => {
-  const [pickedImage, setPickedImage] = useState();
+import Log  from './src/login'
+import Register from './src/register'
 
-  const takeImageHandler = async () => {
-    const image = await ImagePicker.launchCameraAsync({
-      allowsEditing: true,
-      aspect: [16, 9],
-      quality: 0.5
-    });
-
-    setPickedImage(image.uri);
-    props.onImageTaken(image.uri);
-  };
-
+function LOG({ navigation: { navigate } }) {
   return (
-    <View style={styles.imagePicker}>
-      <View style={styles.imagePreview}>
-        {!pickedImage ? (
-          <Text>No image picked yet.</Text>
-        ) : (
-          <Image style={styles.image} source={{ uri: pickedImage }} />
-        )}
-      </View>
-      <Button
-        title="Take Image"
-        onPress={takeImageHandler}
-      />
+    <View >
+      <Log/>
+      <Button title='还没有账号？注册' onPress={() => navigate("注册")} />
     </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  imagePicker: {
-    alignItems: 'center',
-    marginBottom: 15
-  },
-  imagePreview: {
-    width: '100%',
-    height: 200,
-    marginBottom: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#ccc',
-    borderWidth: 1
-  },
-  image: {
-    width: '100%',
-    height: '100%'
+function REGISTER({ navigation: { navigate } }){
+  return(
+    <View>
+      <Register />
+      <Button title='已有账号？登录' onPress={() => navigate("登录")} />
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator();
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName= "登录" headerShown={false}>
+        <Stack.Screen name="登录" component={LOG} />
+        <Stack.Screen name="注册" component={REGISTER} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
   }
-});
+}
 
-export default ImgPicker;
+const styles = StyleSheet.create({})
