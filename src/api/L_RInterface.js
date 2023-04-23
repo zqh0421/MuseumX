45c4ce802b1c31c626923575343e843a48adfa83
zhangran1001@126.com <zhangran1001@126.com>
2023-04-23 23:02:40 +0800

    登录注册接口更新

--- src/api/L_RInterface.js
+++ src/api/L_RInterface.js
@@ -1,114 +1,65 @@
 //登录
 export async function login(username,password) {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        //'Authorization':`Bearer ${apikey}`
-      },
-      body: JSON.stringify({
-        "userAccount": username,
-        "userPassword": password
-      })
-    };
-  
-    try {
-      // fetch('http://101.200.148.39:3458/api/controller/adminUser/login', options)
-      // .then(res => {
-      //   console.log(res)
-      //   res.json().then(data => {
-      //     console.log(data)
-      //   })
-      // })
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "userAccount":username,
+      "userPassword":password
+    })
+  };
 
-      const response = await fetch('http://101.200.148.39:3458/api/controller/adminUser/login', options);
-      
-      if(response.ok){
-        console.log('Request successful');
-      }else{
-        if(response.err==='1'){
-          alert('wrong username!');
-        }else if(response.err==='2'){
-          alert('wrong password!');
-        }else
-        console.log('Request failed'); 
-      }
-
-      const data= await response.json();
-      console.log(data);
-
-    } catch (err) {
-      console.log(err);
+  try {
+    const response = await fetch('http://101.200.148.39:3458/api/controller/adminUser/login', options);
+    if(response.ok){
+      console.log('Request successful');
     }
+    else{
+      if(response.err==="1")
+        console.log('wrong username');
+      else if(response.err==="2")
+        console.log('wrong password');
+      else 
+        console.log('Request failed')
+    }
+    const data=await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
   }
-  //注册
-export async function register(checkPassword,username,password) {
-    const options = {
+}
+//注册
+export async function register(checkpassword,username,password){
+  const options = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        //'Authorization':`Bearer ${apikey}`
       },
       body: JSON.stringify({
-        "checkPassword":checkPassword,
+        "checkPassword":checkpassword,
         "userAccount":username,
         "userPassword":password
       })
-    };
-  
-    try {
+  };
+
+  try {
       const response = await fetch('http://101.200.148.39:3458/api/controller/adminUser/register', options);
-      
       if(response.ok){
         console.log('Request successful');
-      }else{
-        if(response.err==='3'){
-          alert('the username already exists!');
-        }
-        else
-        console.log('Request failed');
       }
-      const data= await response.json();
+      else{
+        if(response.err==="3")
+            console.log('the username alreadly exists');
+        else 
+            console.log('Request failed')
+      }
+      const data=await response.json();
       console.log(data);
-      
     } catch (err) {
       console.log(err);
     }
-  }
+}
 
-  // login("123", "123")
-  register("123","123","123")
-
-async function register(checkPassword,username,password) {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        //'Authorization':`Bearer ${apikey}`
-      },
-      body: JSON.stringify({
-        "checkPassword":checkPassword,
-        "userAccount":username,
-        "userPassword":password
-      })
-    };
-  
-    try {
-      const response = await fetch('http://101.200.148.39:3458/api/controller/adminUser/register', options);
-      
-      if(response.ok){
-        console.log('Request successful');
-      }else{
-        if(response.err==='3'){
-          alert('the username already exists!');
-        }
-        else
-        console.log('Request failed');
-      }
-      const data= await response.json();
-      console.log(data);
-      
-    } catch (err) {
-      console.log(err);
-    }
-  }
+//   login("1234","1234")
