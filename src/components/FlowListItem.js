e62ecf250d601f300c2fb0128fc474929732e5ea
Cynthia1001 <130059747+Cynthia1001@users.noreply.github.com>
2023-05-07 16:26:55 +0800

    Revert "Discover and Profile update"

--- src/components/FlowListItem.js
+++ src/components/FlowListItem.js
@@ -1,21 +1,6 @@
 import { View, Text, StyleSheet,Image } from 'react-native'
-import { Surface,IconButton,MD3Colors} from 'react-native-paper'
-import { useEffect, useState } from 'react'
 import Pic from '../../assets/pic.png'
-
-
 const FlowListItem = (props) => {
-  const [color, setColor] = useState(props.isLoved ? MD3Colors.error60 : MD3Colors.error100)
-  useEffect(() => {
-    // 获取后端数据
-    // setList
-  }, [])
-  const onPressLove = () => {
-    // 1. 点赞按钮样式变化
-    if (color === MD3Colors.error60) setColor(MD3Colors.error100)
-    else if (color === MD3Colors.error100) setColor(MD3Colors.error60)
-    // 2. 向后端发送请求，修改
-  }
   return (
     <View style={styles.itemContainer}>
       <View style={styles.bg} ></View>
@@ -23,32 +8,23 @@ const FlowListItem = (props) => {
         <Image source={Pic} style={styles.image}/>
       </View>
       <Text style={styles.itemTitle} >{props.title}</Text>
-      <Text style={styles.HeadSculpture}></Text>
-      <Text style={styles.itemUsername}>{props.username}</Text>
-      
-      <View style={styles.button}>
-          <IconButton
-            icon="heart"
-            iconColor={color}
-            size={17}
-            onPress={onPressLove}
-          />
-      </View>
-      <Text style={styles.LoveNumber}>{props.likes}</Text>
+      <Text>{props.username}</Text>
+      <Text>{props.time}</Text>
+      <Text>{props.likes}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   itemContainer: {
-    width: '93%',
+    width: '92%',
     borderRadius: 10,
     padding: '5%',
     marginBottom: '5%',
   },
   bg: {
     backgroundColor: '#000',
-    opacity: 0.4,
+    opacity: 0.5,
     overflow: 'hidden',
     borderRadius: 5,
     position: 'absolute',
@@ -69,44 +45,9 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   itemTitle: {
-    top:5,
-    fontSize: 12,
-    margin:2,
-    letterSpacing:1,
-    lineHeight:18,
+    fontSize: 16,
     fontWeight: 'bold',
     color: 'white'
-  },
-  itemUsername: {
-    top:16,
-    right:-28,
-    fontSize: 13,
-    color: '#ccc'
-  },
-  HeadSculpture: {
-    width: 20,
-    height: 20,
-    borderRadius: 20,
-    borderWidth: 1,
-    borderColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#AAA',
-    position: 'absolute',
-    bottom: 8,
-    left: 10,
-  },
-  button: {
-    right:-94,
-    height:0,
-    width:5,
-    top:-13.5,
-  },
-  LoveNumber: {
-    right:-128,
-    fontSize: 12,
-    top:0,
-    color: '#ccc'
   }
 })
 
