61d10ff110d287096e13a63d5cbf171a8c3e3f3b
zhisanji <627236532@qq.com>
2023-05-12 18:01:12 +0800

    接口

--- src/api/myreleaseInterface.js
+++ src/api/myreleaseInterface.js
@@ -1,1 +1,29 @@
 //用户发布的帖子
+export async function myrelease() {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: JSON.stringify({
+        
+      })
+    }
+  
+    try {
+      const response = await fetch(
+        `http://101.200.148.39:3599/api/controller/mood/my.do`,
+        options
+      )
+      if (response.ok) {
+        console.log('Request successful')
+      } else {
+        console.log('Request failed')
+      }
+      const data = await response.json()
+      console.log(data)
+      return data
+    } catch (err) {
+      console.log(err)
+    }
+  }
