f4e8c64728414afcc1005f425577f5f6fb50c621
zqh0421 <zqh0421@hotmail.com>
2023-05-23 12:36:51 +0800

    Fix bugs

---
+++ src/api/editUsername.js
@@ -0,0 +1,30 @@
+export async function editUsername(token,formData) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'token': token
+    },
+    body: formData
+  }
+
+  try {
+    const response = await fetch(
+      'http://101.200.148.39:3599/api/controller/all/user/updateAccount',
+      //'http://101.200.148.39:3599/api/all/user/login',
+      options
+    )
+    if (response.ok) {
+      console.log('Request successful')
+    } else {
+      if (response.status === '1') console.log('wrong username')
+      else if (response.err === '2') console.log('wrong password')
+      else console.log('Request failed')
+    }
+    const data = await response.json()
+    console.log(data)
+    return data
+  } catch (err) {
+    console.log(err)
+  }
+}
